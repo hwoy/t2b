29cfd68b17e7a2411ba575bb1bd0753a5757e4f2
Watthanachai Dueanklang <zaintcorp@yahoo.com>
2015-09-10 16:49:44 +0700

    Fixed main.c

--- main.c
+++ main.c
@@ -3,12 +3,33 @@
 #include "function.h"
 
 #define BSIZE 64
-#define COL 5
+#define COL 16
 #define BASE 10
+#define DELIM ','
 
+void showErr (const char *errstr[], int errNO);
+int showHelp (char *path, const char *param[], const char *pdetail[],
+	      int ret);
+static unsigned int basename (const char *ch);
 
 static char suffix[BSIZE];
 static char buff[BSIZE];
+
+static const char *errstr[] =
+  { "Parameters is not match", "Input file is not found",
+  "Can not assign an output file", "Base number must not be zero"
+};
+static const char *param[] = { "-b" };
+static const char *pdetail[] = { "base Number" };
+
+enum __errNO
+{
+  e_param, e_input, e_output, e_basezero
+};
+enum __param
+{
+  e_base
+};
 
 int
 main (int argc, char *argv[])
@@ -16,31 +37,38 @@ main (int argc, char *argv[])
   FILE *in, *out;
   unsigned int ch, i, j, base;
   static char instr[1024], outstr[1024];
-/*****************************************/
+/******************* Parameters **********************/
 
   if (argc < 2)
     {
-      fprintf (stderr, "Parameter\n");
+      showErr (errstr, e_param);
+      return showHelp (argv[0], param, pdetail, 0);
       return 1;
     }
 
   base = BASE;
   j = 1;
-  if (!strncmp (argv[1], "-b", 2))
+  if (!strncmp (argv[1], param[e_base], sLen (param[e_base])))
     {
       if (!isUint (argv[1] + 2))
 	{
-	  fprintf (stderr, "-b parameter\n");
-	  return 1;
+	  showErr (errstr, e_param);
+	  return showHelp (argv[0], param, pdetail, 0);
 	}
       base = s2ui (argv[1] + 2);
       j = 2;
     }
 
+  if (!base)
+    {
+      showErr (errstr, e_basezero);
+      return showHelp (argv[0], param, pdetail, 0);
+    }
+
   suffix[0] = '.';
   ui2s (base, suffix + 1, BSIZE - 1, 10);
 
-/*****************************************/
+/******************* Parameters **********************/
   for (; j < argc; j++)
     {
       strcpy (instr, argv[j]);
@@ -49,13 +77,13 @@ main (int argc, char *argv[])
 
       if (!(in = fopen (instr, "rb")))
 	{
-	  fprintf (stderr, "inprt %s\n", instr);
+	  showErr (errstr, e_input);
 	  continue;
 	}
 
       if (!(out = fopen (outstr, "wb")))
 	{
-	  fprintf (stderr, "outptr %s\n", outstr);
+	  showErr (errstr, e_output);
 	  fclose (in);
 	  continue;
 	}
@@ -63,7 +91,7 @@ main (int argc, char *argv[])
       while ((ch = fgetc (in)) != EOF)
 	{
 	  ui2s (ch, buff, BSIZE, base);
-	  fprintf (out, "%s,", buff);
+	  fprintf (out, "%s%c", buff, DELIM);
 	  if (++i > COL - 1)
 	    {
 	      fprintf (out, "\n");
@@ -75,4 +103,34 @@ main (int argc, char *argv[])
       fclose (out);
     }
   return 0;
+}
+
+void
+showErr (const char *errstr[], int errNO)
+{
+  fprintf (stderr, "ERR NO:%u = %s\n", errNO, errstr[errNO]);
+}
+
+int
+showHelp (char *path, const char *param[], const char *pdetail[], int ret)
+{
+  fprintf (stderr, "\nUSAGE: %s {%sN} file1 file2 ...\n",
+	   path + basename (path), param[e_base]);
+  fprintf (stderr, "%s = %s\n", param[e_base], pdetail[e_base]);
+
+  return ret;
+}
+
+static unsigned int
+basename (const char *ch)
+{
+  unsigned int i, j;
+  for (i = 0, j = 0; ch[i]; i++)
+    {
+      if (ch[i] == '\\' || ch[i] == '/')
+	{
+	  j = i;
+	}
+    }
+  return (j == 0) ? 0 : j + 1;
 }
